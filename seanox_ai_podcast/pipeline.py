1644bd0db7f62697f3c7621981f4d3fb33d0490a
seanox <service@seanox.com>
2026-03-12 07:24:21 +0100

    #0000 Pipeline: Implementation

--- seanox_ai_podcast/pipeline.py
+++ seanox_ai_podcast/pipeline.py
@@ -14,6 +14,7 @@ PATTERN_SEGMENT_WAV_FILE = re.compile(r"^0x((?:[0-9a-fA-F]{32})+)\.wav$")
 
 def _create_segment_wav(segment: structure.Segment, workspace: Path):
     output = Path(workspace, f"0x{segment.hash()}.wav")
+    # TODO
     output.touch(exist_ok=True)
     print(output)
 
@@ -68,50 +69,3 @@ def pipeline(source: str | Path, workspace: str | Path = None) -> None:
     _mix_podcast_wav(podcast, workspace, target)
 
     LOGGING.info("Done")
-
-# --- --- ---
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
-)
-
-pipeline("../examples/Minimalismus als Design- und Architektur-Entscheidung/podcast.yaml")
-
-# Abstract
-# - Creation of a podcast (wav only) via TTS API
-# x Based on a YAML structure
-# - Minimization of API calls
-#
-# Environment Variables
-# x API Key
-#
-# Arguments
-# x source file
-# x directory output/working (optional)
-#
-# How it works
-# x Parse YAML file into structure object
-#   x Validation + error output (YAML schema)
-# x Create the output directory if it does not exist
-#   x If not specified, output = input directory
-# x Create map with hashes for all segments (all fields)
-#   x The hashing concept is intended to simplify moving without having to
-#     recreate segments or juggle IDs.
-#   x Protection against hash collisions by combining the checksum.
-#   x TODO: Permanent validation due to hash collision?
-#     Error message/warning/do nothing?
-# x Clean up sound fragments if there is no matching hash in YAML
-# - Generate sound fragments (wav format only)
-#   - Create a system prompt from speaker info + segment prompt
-#   x For all segments for which no sound fragment exists for their hash
-#   x Use the hash from the YAML segment as the name
-# - Mix the sound fragments according to their position in YAML into an output wav
-#   - take into account the "offset" which can be positive or negative
-#
-# TODO:
-# - Audio parser.py in YAML
-# x API parser.py (except key in YAML)
-# x YAML support for environment variables
-#   then the API key can be used indirectly in the configuration
